fd0a82d597a8c9a62be351a1615100f905b0a3a6
Ricardas <96791353+r-stake@users.noreply.github.com>
2023-10-31 13:40:28 +0200

    build: Bundled CSS files with Webpack and adjusted main.js file name
    CSS files have been bundled with Webpack, to allow the page to properly load with github pages. As a result, the name of the main JavaScript file has been changed to bundle.js. In the index.js file, two CSS files, normalize.css and style.css, have been imported for use within the project.

--- src/javascript/home.js
+++ src/javascript/home.js
@@ -1,5 +1,4 @@
-import { createReference, createElement, appendElement, addTextContent, addClass, createMainSection } from './helper_functions.js';
-import { createMainSection } from './helper_functions.js';
+import { createReference, createElement, appendElement, addTextContent, createMainSection } from './helper_functions.js';
 
 const home = {
   description: "Discover Autumn Lights, a haven of tranquility. With its softly lit, rustic interior and a serene ambiance that perfectly captures the essence of autumn, our restaurant offers a respite from the everyday hustle. Indulge in our thoughtfully curated menu, artfully prepared to evoke the flavors of the season, while basking in the soothing melodies that add an extra layer of relaxation to your dining experience. At Autumn Lights, let the gentle atmosphere and the delightful cuisine guide you into a moment of peaceful bliss and contentment."
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,3 +1,5 @@
 import { renderPage } from './helper_functions.js';
+import '../css/normalize.css';
+import '../css/style.css';
 
 renderPage();
